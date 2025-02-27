b46a6fe983495c1a671e2cc3590166fcdf76f853
luispdm <17044119+luispdm@users.noreply.github.com>
2025-02-27 10:38:05 +0100

    tokio spawn

--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -4,8 +4,46 @@ use tokio::net::TcpListener;
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    tokio::spawn(async move {
+        echo(first).await
+    });
+
+    tokio::spawn(async move {
+        echo(second).await
+    });
+
+    Ok(())
 }
+
+async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = stream.split();
+            tokio::io::copy(&mut reader, &mut writer).await.map(|_| ())
+        });
+    }
+}
+
+// Teacher's approach:
+// pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
+//     let handle1 = tokio::spawn(echo(first));
+//     let handle2 = tokio::spawn(echo(second));
+//     let (outcome1, outcome2) = tokio::join!(handle1, handle2);
+//     outcome1??;
+//     outcome2??;
+//     Ok(())
+// }
+
+// async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+//     loop {
+//         let (mut socket, _) = listener.accept().await?;
+//         tokio::spawn(async move {
+//             let (mut reader, mut writer) = socket.split();
+//             tokio::io::copy(&mut reader, &mut writer).await.unwrap();
+//         });
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
